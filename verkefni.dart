62dae9dc28e0eba1e0fe440efcac24e12b1f0eb9
KolbeinnNoi <kolbeinnnoi99@gmail.com>
2024-09-30 18:40:46 +0000

    Create verkefni.dart

---
+++ verkefni.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+// Try writing code using a while loop which coutns down to one, 
+// and then prints "Happy New Year". Use a while loop
+
+void main () {
+  // whileLoop();
+  whileLoopAgain();
+}
+
+void whileLoop() {
+
+  int number = 10;
+  while(number >= 0) {
+    print(number);
+    number--;
+  }
+  print("Happy new year!");
+
+}
+
+// Another challenge!
+// Write a program that prints to the user:
+// "Tell me a statement to repeat back to you. Enter 'quit' to end the program"
+// If the user types in anything except 'quit' -print out to the 
+//screen what he entered and then repeat the process. If he types in 'quit' -the program ends.
+
+void whileLoopAgain() {
+
+  // String input = stdin.readLineSync();
+
+}
